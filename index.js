ee714cb2df6178832e8fe1474a15430bd76c1db4
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2021-09-30 15:24:21 +0200

    aaaaaaaaa

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const https = require("https");
 const fs = require("fs");
 
 const app = express();
-const port = 443;
+const port = 3842;
 
 let key = fs.readFileSync(__dirname + "/ssl-cert-snakeoil.key");
 let cert = fs.readFileSync(__dirname + "/ssl-cert-snakeoil.pem");
