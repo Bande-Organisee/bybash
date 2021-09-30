d2f6fd00fdc65526e92031228fd46d5a7071b3e1
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2021-09-30 02:03:21 +0200

    update api response

--- index.js
+++ index.js
@@ -9,30 +9,37 @@ const cors = require("cors");
 app.use(cors());
 
 async function scanMap(ipMap, portMap) {
-  try {
-    const { stdout } = await exec(`nmap -Pn -p ${portMap} ${ipMap}`);
-    return stdout;
-  } catch (err) {
-    return err;
-  }
+    try {
+        const { stdout } = await exec(`nmap -Pn -p ${portMap} ${ipMap}`);
+        return stdout;
+    } catch (err) {
+        return err;
+    }
 }
 
+app.get("/", (req, res) => {
+    console.log("accueil");
+    res.send("Api Bypass");
+});
+
 app.get("/michel", (req, res) => {
-  console.log("michel envoyé");
-  res.send("M I C H E L");
+    console.log("michel envoyé");
+    res.send("M I C H E L");
 });
 
 app.get("/getIp", async (req, res) => {
-  const ipQuery = req.query.ip;
-  const portQuery = req.query.port;
+    const ipQuery = req.query.ip;
+    const portQuery = req.query.port;
 
-  if (ipQuery && portQuery) {
-    const response = await scanMap(ipQuery, portQuery);
-    res.send(response);
-  }
-  console.log("scan envoyé");
+    if (ipQuery && portQuery) {
+        const response = await scanMap(ipQuery, portQuery);
+        res.send(response);
+    } else {
+        res.send("Erreur, champ(s) vide(s). Réessayez");
+    }
+    console.log("scan envoyé");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
